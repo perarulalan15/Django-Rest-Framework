42533bddf5fa31e92cfe4cc729ceeece2fa00005
perarulalan15 <perarulalan01@gmail.com>
2024-10-07 20:52:45 +0530

    Run Django project server

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -1,9 +1,10 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/"
 
 response = requests.get(endpoint, json={"query":"hello world!"})
 print(response.text)
-print(response.json())
 print(response.status_code)
+# print(response.json())
