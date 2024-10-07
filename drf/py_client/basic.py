23973991b3c4b7075c0d261ab53a97a083c3772b
perarulalan15 <perarulalan01@gmail.com>
2024-10-07 20:40:30 +0530

    created python API client

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -1,1 +1,9 @@
 import requests
+
+endpoint = "https://httpbin.org/status/200/"
+endpoint = "https://httpbin.org/anything"
+
+response = requests.get(endpoint, json={"query":"hello world!"})
+print(response.text)
+print(response.json())
+print(response.status_code)
