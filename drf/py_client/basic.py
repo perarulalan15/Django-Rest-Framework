1c1ebaa46725cf2450a04a7bff50c41a3f3dbe5d
perarulalan15 <perarulalan01@gmail.com>
2024-10-11 18:25:28 +0530

    created first api view

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -2,9 +2,9 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://localhost:8000/api/"
 
 response = requests.get(endpoint, json={"query":"hello world!"})
-print(response.text)
-print(response.status_code)
-# print(response.json())
+# print(response.text)
+# print(response.status_code)
+print(response.json())
